96d331e421324c1068a94b64f4166a2335f65938
nadahkhaledd <nadahkhaledd@gmail.com>
2022-04-15 15:45:15 +0200

    predict test data

--- main.py
+++ main.py
@@ -10,20 +10,33 @@ dataset = pd.read_csv('data.csv')
 x = dataset['Hours']
 y = dataset['Scores']
 
+plt.title('Raw Data')
 plt.scatter(x, y, color='black')
 plt.xlabel('Hours')
 plt.ylabel('Scores')
 plt.show()
 
+
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
-
 
 xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.2, random_state=0)
 
 regression = LinearRegression()
 regression.fit(X=xTrain.reshape(-1, 1),  y=yTrain)
-tested = regression.predict(X=xTest)
+
+line = regression.coef_ * x + regression.intercept_
+plt.title('Gradient descent Line fit')
+plt.scatter(x, y, color='black')
+plt.xlabel('Hours')
+plt.ylabel('Scores')
+plt.plot(x, line, color='red')
+plt.show()
+
+
+predicted = regression.predict(X=xTest)
+
+print(predicted)
 
 
 
