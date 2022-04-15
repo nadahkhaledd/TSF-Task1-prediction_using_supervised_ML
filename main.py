ff2b806de7183d389770d7328f30a969f78dd9d6
nadahkhaledd <nadahkhaledd@gmail.com>
2022-04-15 15:21:09 +0200

    fit train and test

--- main.py
+++ main.py
@@ -6,12 +6,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 
-
 dataset = pd.read_csv('data.csv')
-
 x = dataset['Hours']
 y = dataset['Scores']
 
-dataset.plot(x='Hours', y='Scores', style='o')
+plt.scatter(x, y)
+plt.xlabel('hours')
+plt.ylabel('scores')
 plt.show()
+
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+
+xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.2, random_state=0)
+
+regression = LinearRegression()
+regression.fit(X=xTrain.reshape(-1,1),  y=yTrain)
+tested = regression.predict(X=xTest)
+
+
+
+
 
